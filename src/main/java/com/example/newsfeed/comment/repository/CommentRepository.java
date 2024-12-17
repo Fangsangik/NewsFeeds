6475a28d27e7da7a437dbe8990fcce137ed7458c
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-17 19:32:38 +0900

    feat : "CommentRepository" - 댓글 Repo

---
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -0,0 +1,18 @@
+package com.example.newsfeed.comment.repository;
+
+import com.example.newsfeed.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.children WHERE c.feed.id = :feedId")
+    List<Comment> findByFeedId(@Param("feedId") Long feedId);
+
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.children WHERE c.id = :commentId")
+    Optional<Comment> findByIdWithReplies(@Param("commentId") Long commentId);
+}
