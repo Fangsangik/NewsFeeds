92acfb092c89caf7cb93b6879095a77b78f27d2d
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-18 17:12:28 +0900

    refactor : "FeedRequestDto" - image, address, latitude, longitude 필드값 추가 => 카카오 지도 필드값 추가

--- src/main/java/com/example/newsfeed/feed/dto/FeedRequestDto.java
+++ src/main/java/com/example/newsfeed/feed/dto/FeedRequestDto.java
@@ -9,19 +9,32 @@ import lombok.Getter;
 public class FeedRequestDto {
     private String title;
     private String content;
+    private String image;
+    private String address;
+    private Double latitude;
+    private Double longitude;
 
     @Builder
-    public FeedRequestDto(String title, String content) {
+    public FeedRequestDto(String title, String content, String image, String address, Double latitude, Double longitude) {
         this.title = title;
         this.content = content;
+        this.image = image;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
 
-    public static Feed toDto(Member member, FeedRequestDto feedRequestDto) {
+    public static Feed toDto(Member member, FeedRequestDto feedRequestDto, String address) {
         return Feed.builder()
                 .member(member)
                 .title(feedRequestDto.getTitle())
                 .content(feedRequestDto.getContent())
+                .image(feedRequestDto.getImage())
+                .address(feedRequestDto.getAddress())
+                .latitude(feedRequestDto.getLatitude())
+                .longitude(feedRequestDto.getLongitude())
+                .address(address)
                 .build();
     }
 }
