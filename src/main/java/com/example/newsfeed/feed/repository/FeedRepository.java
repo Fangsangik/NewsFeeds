29cf7bec63ac705a1f1ea6eac1ba063a88380407
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 22:18:44 +0900

    refactor : "FeedRepository" - 좋아요를 가져올때 전체 합산 값이 아닌 1로만 count 해서 가져와 sum 로직 추가해서 해결

--- src/main/java/com/example/newsfeed/feed/repository/FeedRepository.java
+++ src/main/java/com/example/newsfeed/feed/repository/FeedRepository.java
@@ -1,13 +1,18 @@
 package com.example.newsfeed.feed.repository;
 
+import com.example.newsfeed.comment.entity.Comment;
+import com.example.newsfeed.feed.dto.FeedWithLikeCountDto;
 import com.example.newsfeed.feed.entity.Feed;
+import com.example.newsfeed.like.entity.Like;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
@@ -15,6 +20,7 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     @Query("SELECT f FROM Feed f WHERE f.member.id = :memberId")
     List<Feed> findFeedsByMemberId(@Param("memberId") Long memberId);
 
-    @Query("select f from Feed f Left Join f.likes l group by f.id order by count(l) desc")
-    Page<Feed> findAllFeedsOrderByLikeCount(Pageable pageable);
+    @Query("SELECT new com.example.newsfeed.feed.dto.FeedWithLikeCountDto(f.id, f.title, f.content, SUM(l.likeCount)) " +
+            "FROM Feed f LEFT JOIN f.likes l GROUP BY f.id ORDER BY SUM(l.likeCount) DESC")
+    Page<FeedWithLikeCountDto> findAllFeedsOrderByLikeCount(Pageable pageable);
 }
