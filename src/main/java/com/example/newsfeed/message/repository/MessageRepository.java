89a8562cd2042049a6c7648adebb4db869adebb6
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 22:25:51 +0900

    refactor : "MessageRepository" - findAllByReceiverIdAndReadStatusFalse JPQL로 refactoring 할 예정

--- src/main/java/com/example/newsfeed/message/repository/MessageRepository.java
+++ src/main/java/com/example/newsfeed/message/repository/MessageRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    Page<MessageRequestDto> findMemberById(Long memberId, Pageable pageable);
+    List<Message> findByReceiverIdAndReadStatusFalse(Long memberId);
+
+    Page<MessageRequestDto> findAllByReceiverIdAndReadStatusFalse(Long memberId, Pageable pageable);
 }
