85831d653e2263823a680bee81689de0123ef0f5
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 22:26:22 +0900

    refactor : "MessageRequestDto" timeStamp 삭제

--- src/main/java/com/example/newsfeed/message/dto/MessageRequestDto.java
+++ src/main/java/com/example/newsfeed/message/dto/MessageRequestDto.java
@@ -12,20 +12,17 @@ public class MessageRequestDto {
     private Long senderId;
     private Long receiverId;
     private String message;
-    private LocalDateTime timeStamp;
 
-    public MessageRequestDto(Long senderId, Long receiverId, String message, LocalDateTime timeStamp) {
+    public MessageRequestDto(Long senderId, Long receiverId, String message) {
         this.senderId = senderId;
         this.receiverId = receiverId;
         this.message = message;
-        this.timeStamp = timeStamp;
     }
 
     public static Message toEntity(Member sender, Member receiver) {
         return Message.builder()
                 .sender(sender)
                 .receiver(receiver)
-                .timestamp(LocalDateTime.now())
                 .readStatus(false)
                 .build();
     }
