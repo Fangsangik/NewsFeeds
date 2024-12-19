005ceb7e4ad7d8bdaa538e5b6c65012c89c2368c
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 22:22:21 +0900

    refactor : "FeedWithLikeCountDto" - Integer -> Long 타입으로 변경

--- src/main/java/com/example/newsfeed/feed/dto/FeedWithLikeCountDto.java
+++ src/main/java/com/example/newsfeed/feed/dto/FeedWithLikeCountDto.java
@@ -7,15 +7,16 @@ import lombok.Getter;
 public class FeedWithLikeCountDto {
     private Long feedId;
     private String title;
-    private Integer likeCount;
+    private String content;
+    private long likeCount;
 
-    public FeedWithLikeCountDto(Long feedId, String title, Integer likeCount) {
+    public FeedWithLikeCountDto(Long feedId, String title, String content, long likeCount) {
         this.feedId = feedId;
         this.title = title;
+        this.content = content;
         this.likeCount = likeCount;
     }
-
     public static FeedWithLikeCountDto toDto(Feed feed) {
-        return new FeedWithLikeCountDto(feed.getId(), feed.getTitle(), feed.getLikeCount());
+        return new FeedWithLikeCountDto(feed.getId(), feed.getTitle(), feed.getContent(), feed.getLikeCount());
     }
 }
