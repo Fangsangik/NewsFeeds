41af4e9b49528c2915c2a4a599e5ea9ff36c421d
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 22:19:22 +0900

    refactor : "FeedResponseDto" - 직렬화 문제가 있어 DTO 값으로 반환

--- src/main/java/com/example/newsfeed/feed/dto/FeedResponseDto.java
+++ src/main/java/com/example/newsfeed/feed/dto/FeedResponseDto.java
@@ -1,12 +1,18 @@
 package com.example.newsfeed.feed.dto;
 
+import com.example.newsfeed.comment.dto.CommentResponseDto;
 import com.example.newsfeed.comment.entity.Comment;
 import com.example.newsfeed.feed.entity.Feed;
+import com.example.newsfeed.like.dto.LikeResponseDto;
 import com.example.newsfeed.like.entity.Like;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 public class FeedResponseDto {
@@ -14,11 +20,11 @@ public class FeedResponseDto {
     private String content;
     private String image;
     private String address;
-    private List<Comment> comments;
-    private List<Like> likes;
+    private List<CommentResponseDto> comments;
+    private List<LikeResponseDto> likes;
 
     @Builder
-    public FeedResponseDto(String title, String content, String image, String address, List<Comment> comments, List<Like> likes) {
+    public FeedResponseDto(String title, String content, String image, String address, List<CommentResponseDto> comments, List<LikeResponseDto> likes) {
         this.title = title;
         this.content = content;
         this.image = image;
@@ -28,13 +34,21 @@ public class FeedResponseDto {
     }
 
     public static FeedResponseDto toDto(Feed feed) {
+        List<CommentResponseDto> comments = (feed.getComments() != null) ? feed.getComments().stream()
+                .map(CommentResponseDto::toDto)
+                .collect(Collectors.toList()) : null;
+
+        List<LikeResponseDto> likes = (feed.getLikes() != null) ? feed.getLikes().stream()
+                .map(LikeResponseDto::toDto)
+                .collect(Collectors.toList()) : null;
+
         return FeedResponseDto.builder()
                 .title(feed.getTitle())
                 .content(feed.getContent())
                 .image(feed.getImage())
                 .address(feed.getAddress())
-                .comments(feed.getComments())
-                .likes(feed.getLikes())
+                .comments(comments)
+                .likes(likes)
                 .build();
     }
 }
