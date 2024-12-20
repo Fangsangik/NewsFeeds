eb53b47a89f23dd40d7a18f75e6596ccd4b7bede
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-20 17:40:05 +0900

    refactor : "MyWebSocketHandler" - senMessage : 클라이언트에게 WebSocket 상태일 때 전송하는 로직

--- src/main/java/com/example/newsfeed/config/MyWebSocketHandler.java
+++ src/main/java/com/example/newsfeed/config/MyWebSocketHandler.java
@@ -48,12 +48,12 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         Long memberId = getMemberId(session);
-        log.info("클라이언트로부터 메시지 수신: {}", memberId, message.getPayload());
+        log.info("메시지 수신: 사용자 ID={}, 메시지={}", memberId, message.getPayload());
     }
 
     // 클라이언트 접속 해제 시
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status)  {
         Long memberId = getMemberId(session);
         if (memberId != null) {
             userSessions.remove(memberId);
@@ -61,24 +61,15 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
         log.info("클라이언트 접속 해제: {}", session.getId());
     }
 
-    @Override
-    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-        Long memberId = getMemberId(session);
-        log.error("클라이언트 {}와 통신 중 에러 발생: {}", memberId, exception.getMessage());
-    }
-
-    public void sendMessageToUser(Long receiverId, String message) {
-        WebSocketSession receiverSession = userSessions.get(receiverId);
-        if (receiverSession != null && receiverSession.isOpen()) {
-            try {
-                receiverSession.sendMessage(new TextMessage(message));
-                log.info("메시지 전송 성공: {}", message);
-            } catch (Exception e) {
-                log.error("메시지 전송 실패: {}", e.getMessage());
-            }
-        } else {
-            log.warn("WebSocket 세션이 존재하지 않음: 사용자 ID {}", receiverId);
-        }
+    // 클라이언트에게 메시지 전송
+    public void senMessage(WebSocketSession session, String message) {
+       if (session.isOpen()) {
+           try {
+               session.sendMessage(new TextMessage(message));
+           } catch (Exception e) {
+               log.error("메시지 전송 오류: {}", e.getMessage());
+           }
+       }
     }
 
     private static Long getMemberId(WebSocketSession session) {
