5d606e49de43cce6d0f48de39e21520dded0809e
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-20 21:31:10 +0900

    refactor : "MessageRepository" - JPQL : 쿼리로 좀더 확실하게 처리

--- src/main/java/com/example/newsfeed/message/repository/MessageRepository.java
+++ src/main/java/com/example/newsfeed/message/repository/MessageRepository.java
@@ -5,11 +5,18 @@ import com.example.newsfeed.message.entity.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByReceiverIdAndReadStatusFalse(Long memberId);
 
+    @Query("SELECT new com.example.newsfeed.message.dto.MessageRequestDto(m.sender.id, m.receiver.id, m.message)" +
+            " FROM Message m WHERE m.receiver.id = :memberId AND m.readStatus = false")
     Page<MessageRequestDto> findAllByReceiverIdAndReadStatusFalse(Long memberId, Pageable pageable);
+
+    @Query("SELECT new com.example.newsfeed.message.dto.MessageRequestDto(m.sender.id, m.receiver.id, m.message)" +
+            " FROM Message m WHERE m.receiver.id = :memberId")
+    Page<MessageRequestDto> findAllByReceiverId(Long memberId, Pageable pageable);
 }
