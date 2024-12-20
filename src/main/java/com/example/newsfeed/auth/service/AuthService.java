b06d4b75d10a0acb66777b6368947058cd5f1e25
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-20 17:35:40 +0900

    refactor : "AuthService" - login : 이미 로그인이 되어 있다면 로그아웃 후 다른 아이디로 로그인 할 수 있게 수정 - reissueToken : accessToken 재발급

--- src/main/java/com/example/newsfeed/auth/service/AuthService.java
+++ src/main/java/com/example/newsfeed/auth/service/AuthService.java
@@ -2,40 +2,67 @@ package com.example.newsfeed.auth.service;
 
 import com.example.newsfeed.auth.dto.AuthLoginDto;
 import com.example.newsfeed.auth.dto.LoginRequestDto;
-import com.example.newsfeed.auth.jwt.JwtProvider;
+import com.example.newsfeed.auth.jwt.entity.JwtToken;
+import com.example.newsfeed.auth.jwt.repository.TokenRepository;
+import com.example.newsfeed.auth.jwt.service.JwtProvider;
 import com.example.newsfeed.auth.type.LoginType;
+import com.example.newsfeed.config.PasswordEncoder;
 import com.example.newsfeed.member.entity.Member;
 import com.example.newsfeed.member.repository.MemberRepository;
 import com.example.newsfeed.member.type.Role;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.Optional;
+
 @Service
 public class AuthService {
 
     private final MemberRepository memberRepository;
     private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenRepository tokenRepository;
 
 
-    public AuthService(MemberRepository memberRepository, JwtProvider jwtProvider) {
+    public AuthService(MemberRepository memberRepository, JwtProvider jwtProvider, PasswordEncoder passwordEncoder, TokenRepository tokenRepository) {
         this.memberRepository = memberRepository;
         this.jwtProvider = jwtProvider;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenRepository = tokenRepository;
     }
 
     @Transactional
-    public AuthLoginDto login(LoginRequestDto requestDto) {
+    public void login(LoginRequestDto requestDto) {
+
         Member member = memberRepository.findByEmail(requestDto.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("Email does not exist"));
 
-        if (member.getPassword().equals(requestDto.getPassword())) {
+        if (!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) {
             throw new IllegalArgumentException("Password does not match");
         }
 
-        jwtProvider.generateTokens(member.getId(), Role.USER, LoginType.NORMAL_USER);
+        if (tokenRepository.existsByMemberEmailNot(requestDto.getEmail())) {
+            throw new IllegalStateException("You are already logged in with another account. Please log out first.");
+        }
 
-        return AuthLoginDto.builder()
-                .email(member.getEmail())
+
+        // 3. 기존 토큰 삭제 (PK를 사용)
+        Optional<JwtToken> existingToken = tokenRepository.findByMemberId(member.getId());
+        existingToken.ifPresent(tokenRepository::delete);
+
+        Map<String, String> tokens = jwtProvider.generateTokens(member.getId(), Role.USER, LoginType.NORMAL_USER);
+
+        JwtToken newJwtToken = JwtToken.builder()
+                .member(member)
+                .accessToken(tokens.get("accessToken"))
+                .refreshToken(tokens.get("refreshToken"))
+                .expiredAt(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7))
+                .issuedAt(new Date(System.currentTimeMillis()))
                 .build();
+
+        tokenRepository.save(newJwtToken);
     }
 
     public String refreshAccessToken(String refreshToken) {
@@ -43,6 +70,19 @@ public class AuthService {
             throw new IllegalArgumentException("Invalid Refresh Token");
         }
         Long userId = jwtProvider.getUserIdFromToken(refreshToken);
-        return jwtProvider.generateTokens(Long.valueOf(userId), Role.USER, LoginType.NORMAL_USER).get("accessToken");
+        return jwtProvider.generateTokens(userId, Role.USER, LoginType.NORMAL_USER).get("accessToken");
+    }
+
+    // 개발 단계에서만 사용 예정
+    public String reissueToken(String email, String password) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Email does not exist"));
+
+        //  저절로 인코딩 값이 들어가는데, refresh token이 만료되었을 때, 인코딩된 password가 들어가야 하는지 ??
+        if (!member.getPassword().equals(password)) {
+            throw new IllegalArgumentException("Password does not match");
+        }
+
+        return jwtProvider.generateTokens(member.getId(), Role.USER, LoginType.NORMAL_USER).get("accessToken");
     }
 }
