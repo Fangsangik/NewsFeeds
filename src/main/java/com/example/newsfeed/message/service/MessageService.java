1898d134754720fd70919eb4f3e2478771d5dc9b
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-20 17:39:29 +0900

    refactor : "sendMessage" - 검증 로직 따로 분리 : 추후에 유지 보수성을 생각하면 분리하는게 필요 할 것 같다는 생각이 듬 - sendToWebSocket : 이 또한 마찮가지

--- src/main/java/com/example/newsfeed/message/service/MessageService.java
+++ src/main/java/com/example/newsfeed/message/service/MessageService.java
@@ -32,27 +32,8 @@ public class MessageService {
 
     @Transactional
     public void sendMessage(MessageRequestDto requestDto) {      // 메시지 생성 및 저장
-        Message message = new Message(
-                requestDto.getMessage(),
-                memberRepository.findById(requestDto.getReceiverId())
-                        .orElseThrow(() -> new IllegalArgumentException("받는 사용자가 존재하지 않습니다.")),
-                memberRepository.findById(requestDto.getSenderId())
-                        .orElseThrow(() -> new IllegalArgumentException("보내는 사용자가 존재하지 않습니다.")),
-                false
-        );
-        messageRepository.save(message);
-
-        WebSocketSession receiverSession = userSessions.get(requestDto.getReceiverId());
-        if (receiverSession != null && receiverSession.isOpen()) {
-            try {
-                receiverSession.sendMessage(new TextMessage(requestDto.getMessage()));
-                log.info("메시지가 WebSocket을 통해 전송되었습니다: {}", requestDto.getMessage());
-            } catch (Exception e) {
-                log.error("WebSocket 메시지 전송 실패: {}", e.getMessage());
-            }
-        } else {
-            log.info("사용자가 오프라인 상태입니다. 메시지는 DB에 저장되었습니다.");
-        }
+        Message message = saveMessage(requestDto);
+        sendToWebSocket(requestDto.getReceiverId(), requestDto.getMessage());
     }
 
     @Transactional
@@ -66,5 +47,32 @@ public class MessageService {
     @Transactional(readOnly = true)
     public Page<MessageRequestDto> getMessages(Long memberId, Pageable pageable) {
         return messageRepository.findAllByReceiverIdAndReadStatusFalse(memberId, pageable);
+    }
+
+
+    // 메시지 전송 및 저장
+    private Message saveMessage (MessageRequestDto messageRequestDto) {
+        return messageRepository.save(new Message(
+                messageRequestDto.getMessage(),
+                memberRepository.findById(messageRequestDto.getReceiverId())
+                        .orElseThrow(() -> new IllegalArgumentException("받는 사용자가 존재하지 않습니다.")),
+                memberRepository.findById(messageRequestDto.getSenderId())
+                        .orElseThrow(()
+                        -> new IllegalArgumentException("보내는 사용자가 존재하지 않습니다.")), false));
+    }
+
+    // 추후 재사용성을 높일 수 있도록 분리
+    private void sendToWebSocket(Long receiverId, String message) {
+        WebSocketSession receiverSession = userSessions.get(receiverId);
+        if (receiverSession != null && receiverSession.isOpen()) {
+            try {
+                receiverSession.sendMessage(new TextMessage(message));
+                log.info("메시지가 WebSocket을 통해 전송되었습니다: {}", message);
+            } catch (Exception e) {
+                log.error("WebSocket 메시지 전송 실패: {}", e.getMessage());
+            }
+        } else {
+            log.info("사용자가 오프라인 상태입니다. 메시지는 DB에 저장되었습니다.");
+        }
     }
 }
