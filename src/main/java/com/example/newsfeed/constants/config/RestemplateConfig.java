9dcf7e35448749e991956d4904e56ab1a71a1376
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-27 10:25:48 +0900

    feat : "RestemplateConfig" - package 위치 변경

--- src/main/java/com/example/newsfeed/constants/config/RestemplateConfig.java
+++ src/main/java/com/example/newsfeed/constants/config/RestemplateConfig.java
@@ -1,4 +1,4 @@
-package com.example.newsfeed.config;
+package com.example.newsfeed.constants.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
