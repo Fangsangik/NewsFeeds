9c904d4914fdc75fd7b91fe5eb41240a66e29718
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-06 20:15:52 +0900

    refactor : "DelegatedAuthenticationEntryPoint" - 인증 실패에 대한 응답을 구헝

---
+++ src/main/java/com/example/newsfeed/auth/DelegatedAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.example.newsfeed.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+/**
+ * 인증 실패에 대한 응답을 구헝
+ */
+@Component
+public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * SpringSecurity 예외를 처리하기 위한 resolver
+     */
+    private final HandlerExceptionResolver resolver;
+
+    public DelegatedAuthenticationEntryPoint(HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        resolver.resolveException(request, response, null, authException);
+    }
+}
