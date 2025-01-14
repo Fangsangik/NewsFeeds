d8d1557ecc7e2f8ddb4c9ce44377ae019f160242
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-01-14 12:00:51 +0900

    refactor: "FeedController" - AuthenticatedMemberUtil를 추가해 사용자가 맞는 검증

--- src/main/java/com/example/newsfeed/feed/controller/FeedController.java
+++ src/main/java/com/example/newsfeed/feed/controller/FeedController.java
@@ -1,5 +1,6 @@
 package com.example.newsfeed.feed.controller;
 
+import com.example.newsfeed.auth.util.AuthenticatedMemberUtil;
 import com.example.newsfeed.constants.response.CommonResponse;
 import com.example.newsfeed.feed.dto.FeedRequestDto;
 import com.example.newsfeed.feed.dto.FeedResponseDto;
@@ -23,9 +24,9 @@ public class FeedController {
     }
 
     @PostMapping
-    public ResponseEntity<CommonResponse<FeedResponseDto>> createFeed
-            (@RequestAttribute(name = "memberId") Long memberId,
-             @RequestBody FeedRequestDto feedRequestDto) {
+    public ResponseEntity<CommonResponse<FeedResponseDto>> createFeed (@RequestBody FeedRequestDto feedRequestDto) {
+        Long memberId = AuthenticatedMemberUtil.getAuthenticatedMemberId();
+
         FeedResponseDto feed = feedService.createFeed(memberId, feedRequestDto);
         System.out.println("Received MemberId: " + memberId);
         return ResponseEntity.ok(new CommonResponse<>("피드 작성 완료", feed));
@@ -33,42 +34,44 @@ public class FeedController {
 
 
     @GetMapping("/{feedId}")
-    public ResponseEntity<CommonResponse<FeedResponseDto>> getFeed
-            (
-             @PathVariable Long feedId) {
+    public ResponseEntity<CommonResponse<FeedResponseDto>> getFeed (@PathVariable Long feedId) {
+        Long memberId = AuthenticatedMemberUtil.getAuthenticatedMemberId();
+
         FeedResponseDto feed = feedService.getFeed(feedId);
         return ResponseEntity.ok(new CommonResponse<>("피드 조회 완료", feed));
     }
 
     // 위치기 변경 되어야 하는거 아닌가 라는 의심
     // 회원 기반 피드 조회
-    @GetMapping("/member/{memberId}")
-    public ResponseEntity<CommonResponse<List<FeedResponseDto>>> getFeedsByMemberId(@PathVariable Long memberId) {
+    @GetMapping
+    public ResponseEntity<CommonResponse<List<FeedResponseDto>>> getFeedsByMemberId() {
+        Long memberId = AuthenticatedMemberUtil.getAuthenticatedMemberId();
+
         List<FeedResponseDto> feeds = feedService.getFeedsByMemberId(memberId);
         return ResponseEntity.ok(new CommonResponse<>("회원 기반 피드 조회 완료", feeds));
     }
 
     @GetMapping("/likecount")
-    public ResponseEntity<CommonResponse<Page<FeedWithLikeCountDto>>> getAllCounts
-            (@RequestParam (defaultValue = "0") int page,
-             @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<CommonResponse<Page<FeedWithLikeCountDto>>> getAllCounts(@RequestParam (defaultValue = "0") int page,
+                                                                                   @RequestParam(defaultValue = "10") int size) {
         Page<FeedWithLikeCountDto> feeds = feedService.getAllFeedsOrderByLikeCount(page, size);
         return ResponseEntity.ok(new CommonResponse<>("좋아요 수 기반 피드 조회 완료", feeds));
 
     }
     @PatchMapping("/{feedId}")
-    public ResponseEntity<CommonResponse<FeedUpdateResponseDto>> updateFeed
-            (@RequestAttribute(name = "memberId") Long memberId,
-             @PathVariable Long feedId,
-             @RequestBody FeedRequestDto feedRequestDto) {
+    public ResponseEntity<CommonResponse<FeedUpdateResponseDto>> updateFeed(@PathVariable Long feedId,
+                                                                            @RequestBody FeedRequestDto feedRequestDto) {
+        Long memberId = AuthenticatedMemberUtil.getAuthenticatedMemberId();
+
         FeedUpdateResponseDto updateFeed = feedService.updateFeed(feedId, feedRequestDto);
         return ResponseEntity.ok(new CommonResponse<>("피드 수정 완료", updateFeed));
     }
 
     @DeleteMapping("/{feedId}")
-    public ResponseEntity<CommonResponse<Void>> deleteFeed
-            (@RequestAttribute(name = "memberId") Long memberId,
-             @PathVariable Long feedId) {
+    public ResponseEntity<CommonResponse<Void>> deleteFeed (@PathVariable Long feedId) {
+
+        Long memberId = AuthenticatedMemberUtil.getAuthenticatedMemberId();
+
         feedService.deleteFeed(feedId);
         return ResponseEntity.ok(new CommonResponse<>("피드 삭제 완료"));
     }
