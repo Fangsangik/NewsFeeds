9a681cfb0aa24184f08ba834979128e72e816fe0
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-20 13:34:03 +0900

    refactor : "MemberRepository" - findByIdOrElseThrow : 중복 코드가 발생하기에 deafult 처리

--- src/main/java/com/example/newsfeed/member/repository/MemberRepository.java
+++ src/main/java/com/example/newsfeed/member/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.example.newsfeed.member.repository;
 
+import com.example.newsfeed.exception.ErrorCode;
+import com.example.newsfeed.exception.NotFoundException;
 import com.example.newsfeed.kakao.entity.KakaoMember;
 import com.example.newsfeed.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+
+import static com.example.newsfeed.exception.ErrorCode.NOT_FOUND_MEMBER;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // PK와 삭제되지 않은 조건으로 멤버 조회
@@ -22,4 +26,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByFeedId(@Param("feedId") Long feedId);
 
     Optional<Member> findByKakaoMember(KakaoMember kakaoMember);
+
+    default Member findByIdOrElseThrow(Long id) {
+        return findByIdAndNotDeleted(id).orElseThrow(() -> new NotFoundException(NOT_FOUND_MEMBER));
+    }
 }
