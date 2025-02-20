dc887334fe20915e83966aafdec64727160cc6c4
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-20 14:00:17 +0900

    feat : "MessageRepository" - 메시지를 보낸 특정 회원 조회

--- src/main/java/com/example/newsfeed/message/repository/MessageRepository.java
+++ src/main/java/com/example/newsfeed/message/repository/MessageRepository.java
@@ -1,22 +1,11 @@
 package com.example.newsfeed.message.repository;
 
-import com.example.newsfeed.message.dto.MessageRequestDto;
 import com.example.newsfeed.message.entity.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByReceiverIdAndReadStatusFalse(Long memberId);
 
-    @Query("SELECT new com.example.newsfeed.message.dto.MessageRequestDto(m.sender.id, m.receiver.id, m.message)" +
-            " FROM Message m WHERE m.receiver.id = :memberId AND m.readStatus = false")
-    Page<MessageRequestDto> findAllByReceiverIdAndReadStatusFalse(Long memberId, Pageable pageable);
-
-    @Query("SELECT new com.example.newsfeed.message.dto.MessageRequestDto(m.sender.id, m.receiver.id, m.message)" +
-            " FROM Message m WHERE m.receiver.id = :memberId")
-    Page<MessageRequestDto> findAllByReceiverId(Long memberId, Pageable pageable);
+    Page<Message> findByMemberId(Long memberId, Pageable pageable);
 }
