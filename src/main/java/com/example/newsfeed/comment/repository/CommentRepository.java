37a1d0671f8aef5d595426d07334042cd3e5ed29
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-20 13:46:50 +0900

    refactor : "CommentRepository" - findByIdOrElseThrow : 중복 코드 발생해 default 처리

--- src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/newsfeed/comment/repository/CommentRepository.java
@@ -1,12 +1,15 @@
 package com.example.newsfeed.comment.repository;
 
 import com.example.newsfeed.comment.entity.Comment;
+import com.example.newsfeed.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.example.newsfeed.exception.ErrorCode.NOT_FOUND_COMMENT;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
@@ -19,4 +22,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT c FROM Comment c " +
             "JOIN fetch c.feed f JOIN fetch c.member m WHERE c.id = :commentId")
     Optional<Comment> findByIdWithFeedAndMember(@Param("commentId") Long commentId);
+
+    default Comment findByIdOrElseThrow(Long commentId) {
+        return findByIdWithFeedAndMember(commentId)
+                .orElseThrow(() -> new NotFoundException(NOT_FOUND_COMMENT));
+    }
 }
