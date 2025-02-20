51642b696445ea69f9622ae18a606fe9d1766ef6
FANG <107174112+Fangsangik@users.noreply.github.com>
2025-02-20 13:59:34 +0900

    feat : "MessageServiceImpl" - 메시지를 발송시 Redis에 저장하는 것 뿐만 아닌, 관계형 DB에도 함께 저장 - 보낸 메시지 조회

--- src/main/java/com/example/newsfeed/message/service/MessageService.java
+++ src/main/java/com/example/newsfeed/message/service/MessageService.java
@@ -1,85 +1,11 @@
 package com.example.newsfeed.message.service;
 
-import com.example.newsfeed.exception.ErrorCode;
-import com.example.newsfeed.exception.NotFoundException;
-import com.example.newsfeed.member.repository.MemberRepository;
+import com.example.newsfeed.member.entity.Member;
 import com.example.newsfeed.message.dto.MessageRequestDto;
-import com.example.newsfeed.message.entity.Message;
-import com.example.newsfeed.message.repository.MessageRepository;
-import lombok.extern.slf4j.Slf4j;
+import com.example.newsfeed.message.dto.MessageResponseDto;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.util.Map;
-
-import static com.example.newsfeed.exception.ErrorCode.*;
-
-@Slf4j
-@Service
-public class MessageService {
-
-    private final MessageRepository messageRepository;
-    private final MemberRepository memberRepository;
-    private final Map<Long, WebSocketSession> userSessions;
-
-    public MessageService(MessageRepository messageRepository, MemberRepository memberRepository, Map<Long, WebSocketSession> userSessions) {
-        this.messageRepository = messageRepository;
-        this.memberRepository = memberRepository;
-        this.userSessions = userSessions;
-    }
-
-    @Transactional
-    public void sendMessage(MessageRequestDto requestDto) {      // 메시지 생성 및 저장
-        Message message = saveMessage(requestDto);
-        sendToWebSocket(requestDto.getReceiverId(), requestDto.getMessage());
-    }
-
-    @Transactional
-    public void markAsRead(Long messageId) {
-        Message message = messageRepository.findById(messageId)
-                .orElseThrow(() -> new NotFoundException(NOT_FOUND_MESSAGE));
-        message.setReadStatus(true);
-        messageRepository.save(message);
-    }
-
-    @Transactional(readOnly = true)
-    public Page<MessageRequestDto> getMessages(Long memberId, Pageable pageable) {
-        return messageRepository.findAllByReceiverIdAndReadStatusFalse(memberId, pageable);
-    }
-
-    @Transactional(readOnly = true)
-    public Page<MessageRequestDto> readMessage(Long memberId, Pageable pageable) {
-        return messageRepository.findAllByReceiverId(memberId, pageable);
-    }
-
-
-    // 메시지 전송 및 저장
-    private Message saveMessage (MessageRequestDto messageRequestDto) {
-        return messageRepository.save(new Message(
-                messageRequestDto.getMessage(),
-                memberRepository.findById(messageRequestDto.getReceiverId())
-                        .orElseThrow(() -> new NotFoundException(NOT_FOUND_MEMBER)),
-                memberRepository.findById(messageRequestDto.getSenderId())
-                        .orElseThrow(()
-                        -> new NotFoundException(NOT_FOUND_MEMBER)), false));
-    }
-
-    // 추후 재사용성을 높일 수 있도록 분리
-    private void sendToWebSocket(Long receiverId, String message) {
-        WebSocketSession receiverSession = userSessions.get(receiverId);
-        if (receiverSession != null && receiverSession.isOpen()) {
-            try {
-                receiverSession.sendMessage(new TextMessage(message));
-                log.info("메시지가 WebSocket을 통해 전송되었습니다: {}", message);
-            } catch (Exception e) {
-                log.error("WebSocket 메시지 전송 실패: {}", e.getMessage());
-            }
-        } else {
-            log.info("사용자가 오프라인 상태입니다. 메시지는 DB에 저장되었습니다.");
-        }
-    }
+public interface MessageService {
+    MessageResponseDto sendMessage(Member member, MessageRequestDto messageRequestDto);
+    Page<MessageResponseDto> getMessages(Long memberId, int page, int size);
 }
